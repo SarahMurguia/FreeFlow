f1d2bb5dcdc6da26f0bbe8cea8de29fc4e0b38b9
Sarah Murguia <murguiasarah@gmail.com>
2019-10-30 17:10:05 -0400

    button on signin

---
+++ extension/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkSignUp = document.getElementById('signup');
+    // onClick's logic below:
+    checkSignUp.addEventListener('click', function() {
+        alert("Got it");
+    }, false);
+}, false);
+
