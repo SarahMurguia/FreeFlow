d17f2d2d4efa836ad4f747b65952d6b7da0a92a6
Sarah Murguia <murguiasarah@gmail.com>
2019-10-30 18:23:33 -0400

    sign up checks that passwords match

---
+++ extension/signup.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var completeSignUp = document.getElementById("su-enter");
+
+    completeSignUp.addEventListener('click', function() {
+        enterOk = "false";
+        // Get entered input
+        var email = document.getElementById("su-email").value.indexOf("@");
+        var username = document.getElementById("su-name").value;
+        var pass1 = document.getElementById("su-pass1").value;
+        var pass2 = document.getElementById("su-pass2").value;
+
+        if (pass1 == pass2){
+            enterOk = "true"
+        }
+   
+        if (enterOk == "true"){
+            window.location.href="servicepage.html";
+        }
+        else{
+            alert("Passwords dont match!");
+        }
+
+    }, false);
+}, false);
+
