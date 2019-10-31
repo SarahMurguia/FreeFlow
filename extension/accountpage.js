dfe1586357d2be341f32915e63ec8dee4fc9c802
Sarah Murguia <murguiasarah@gmail.com>
2019-10-31 17:19:46 -0400

    logout button

--- extension/accountpage.js
+++ extension/accountpage.js
@@ -1,3 +1,14 @@
+// Event Listener for Logging out
+document.addEventListener('DOMContentLoaded', function() {
+    var checkLogOut = document.getElementById('ap-logout');
+    checkLogOut.addEventListener('click', function() {
+        // TODO: Link Proper Terms of Service Document
+        window.location.href="popup.html";
+        chrome.browserAction.setPopup({popup: "popup.html"});
+    }, false);
+}, false);
+
+
 // Event Listener for the Delete Account
 document.addEventListener('DOMContentLoaded', function() {
     var checkDelete = document.getElementById('ap-delete');
@@ -8,14 +19,11 @@ document.addEventListener('DOMContentLoaded', function() {
             chrome.browserAction.setPopup({popup: "popup.html"});
 	    } 
         else {
-    	    txt = "You pressed Cancel!";
             window.location.href="accountpage.html";
-
     	}
-
-
     }, false);
 }, false);
+
 
 // Event Listener for Managing Household
 document.addEventListener('DOMContentLoaded', function() {
@@ -34,10 +42,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }, false);
 }, false);
 
+
 // Event Listener for viewing Terms of Service
 document.addEventListener('DOMContentLoaded', function() {
     var checkToS = document.getElementById('ap-tos');
     checkToS.addEventListener('click', function() {
-        window.location.href="signup.html";
+        // TODO: Link Proper Terms of Service Document
+        alert("Pass Through Terms of Service Document \n ~~~~~~~~~ \n ~~~~~~~~~ \n ~~~~~~~~~ \n ~~~~~~~~~ \n ~~~~~~~~~");
     }, false);
 }, false);
