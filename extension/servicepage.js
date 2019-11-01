2a98241f0d1a51ddb77efe05454250f7020540b3
Sarah Murguia <murguiasarah@gmail.com>
2019-11-01 02:10:27 -0400

    refactor to folders

--- extension/servicepage.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var toAccount = document.getElementById('sp-settings');
-    // onClick's logic below:
-    toAccount.addEventListener('click', function() {
-    	alert("in here");
-        window.location.href="accountpage.html";
-    }, false);
-}, false);
