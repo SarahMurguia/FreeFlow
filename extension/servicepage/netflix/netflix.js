721137ffba648b62409e8442a8cbcbb21e0b16a1
Sarah Murguia <murguiasarah@gmail.com>
2019-11-04 03:01:12 -0500

    extra deleted

--- extension/servicepage/netflix/netflix.js
+++ extension/servicepage/netflix/netflix.js
@@ -22,35 +22,12 @@ $(function() {
 		}
 	});
 
-	// Login button handler
-	$('#netflix-enter').click(function() {
-
-		// Get form value
-		var acc_user = $('#user').val().trim(),
-			acc_pass = md5($('#pass').val().trim());	// password converted into md5
-
-		// Credential verification
-		$.post('http://freeflow.tk/login.php', {user: user, pass: pass}).done(function(ret) {
-			var id = parseInt(ret);
-			
-		// TODO: Add Acc user and acc pass
-		window.location ='/servicepage/servicepage.html';
-	
-
-		// Connect failed
-		}).fail(function(xhr, status, error) {
-			alert(xhr.responseText);				
-		});
-	});
-
-
-
 	 $('#netflix-enter').click(function() {
         var serv = "http://freeflow.tk/query.php"
 
         // Get entered input
         var email = $('#n-email').val(),
-            pass2 = $('#n-pass').val();
+            pass = $('#n-pass').val();
 		var done = false;
 		$.ajaxSetup({async: false});
         // Check email
