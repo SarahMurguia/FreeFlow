7acc6a1da71d00868356a0c9d5316185c7c0d7b0
wakh <khamwa92@gmail.com>
2019-11-04 00:50:26 -0500

    Sign-up

--- extension/signuppage/signup.js
+++ extension/signuppage/signup.js
@@ -1,27 +1,60 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var completeSignUp = document.getElementById("su-enter");
+$(function() {
+    // Check if form filled to enable login button
+    $('form input').keyup(function() {
+        var empty = false;
+        $('form input').each(function() {
+            if($(this).val() == '') { empty = true; }
+        });
+        if (empty) { $('#su-enter').prop('disabled', true); }
+        else { $('#su-enter').prop('disabled', false); }
+    });
 
-    completeSignUp.addEventListener('click', function() {
-        enterOk = "false";
+    // Handler enter key pressed in any input
+    $('form input').keypress(function(e) {
+        if (e.keyCode == 13) {
+            var empty = false;
+            $('form input').each(function() {
+                if($(this).val() == '') { empty = true; }
+            });
+
+            if (!empty) { $('#login').click() }
+        }
+    });
+
+    $('#su-enter').click(function() {
+        var serv = "http://freeflow.tk/query.php"
         // Get entered input
-        var email = document.getElementById("su-email").value.indexOf("@");
-        var username = document.getElementById("su-name").value;
-        var pass1 = document.getElementById("su-pass1").value;
-        var pass2 = document.getElementById("su-pass2").value;
+        var email = $('#su-email').val(),
+            user = $('#su-name').val(),
+            pass1 = $('#su-pass1').val(),
+            pass2 = $('#su-pass2').val();
+		var done = false;
 
-        // TODO: Need to some add this info to the database
-        if (pass1 == pass2){
-            enterOk = "true"
+		$.ajaxSetup({async: false});
+        // Check password
+        if (pass1 == pass2) {
+            // Check email
+            $.post(serv, { query: "SELECT * FROM users WHERE email='" + email + "';" }, function(ret) {
+				alert(ret);
+                if (ret == ' []') {
+					// Check username
+                    $.post(serv, { query: "SELECT * FROM users WHERE user='" + user + "';" }, function(ret) {
+						alert(ret);
+                        if (ret == ' []') {
+							var pass = md5(pass1);
+                            $.post(serv, {query: "INSERT INTO users (user, pass, email) VALUES ('" + user + "', '" + pass + "', '" + email + "');"}, function() {});
+                            window.location.href="/servicepage/servicepage.html";
+                            chrome.browserAction.setPopup({popup: "/servicepage/servicepage.html"});
+                        } else {
+                            alert('Username have been used!\n');
+                        }
+                    });
+                } else {
+					alert('Email have been used!\n');
+                }
+            });
+        } else {
+            alert('Password do not match!\n');
         }
-   
-        if (enterOk == "true"){
-            window.location.href="/servicepage/servicepage.html";
-            chrome.browserAction.setPopup({popup: "/servicepage/servicepage.html"});
-        }
-        else{
-            alert("Passwords dont match!");
-        }
-
-    }, false);
-}, false);
-
+    });
+});
