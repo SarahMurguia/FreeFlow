2c7ed78b0d86cef4b7305378cf98cbc040f637e5
Sarah Murguia <murguiasarah@gmail.com>
2019-11-03 22:57:08 -0500

    activated service buggy

--- extension/servicepage/servicepage.js
+++ extension/servicepage/servicepage.js
@@ -1,7 +1,45 @@
+// Switch to account settings
 document.addEventListener('DOMContentLoaded', function() {
     var toAccount = document.getElementById('sp-settings');
-    // onClick's logic below:
     toAccount.addEventListener('click', function() {
         window.location.href="/accountpage/accountpage.html";
+    }, false);
+}, false);
+
+
+// Netflix Listener
+document.addEventListener('DOMContentLoaded', function() {
+    var netflixCheck = document.getElementById('sp-netflix');
+    netflixCheck.addEventListener('click', function() {
+    	alert("netflix");
+    	if (netflixCheck.classList.contains("activated") == false){
+    		netflixCheck.classList.add("activated");
+    	}
+
+    }, false);
+}, false);
+
+// Hulu Listener
+document.addEventListener('DOMContentLoaded', function() {
+    var huluCheck = document.getElementById('sp-hulu');
+    huluCheck.addEventListener('click', function() {
+        alert("hulu");
+        if (huluCheck.classList.contains("activated") == false){
+    		huluCheck.classList.add("activated");
+    	}
+    }, false);
+}, false);
+
+
+// HBO Listener
+document.addEventListener('DOMContentLoaded', function() {
+    var hboCheck = document.getElementById('sp-hbo');
+
+    hboCheck.addEventListener('click', function() {
+        alert("hbo");
+        if (hboCheck.classList.contains("activated") == false){
+    		hboCheck.classList.add("activated");
+    	}
+
     }, false);
 }, false);
