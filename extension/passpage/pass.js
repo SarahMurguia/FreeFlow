0bfcbc425fd7e73c9e1f9dfbddc20bb13485e489
Claire McManus <claireymcmanus@gmail.com>
2019-11-21 18:31:43 -0500

    added change password, update in database bot working

--- extension/passpage/pass.js
+++ extension/passpage/pass.js
@@ -10,12 +10,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (pass1 == pass2){
             // TODO: Check that curr pass matches database
-            enterOk = "true";
+            chrome.storage.sync.get("user_id", function(result) {
+                if(!chrome.runtime.error) {
+                    var serv = "http://freeflow.tk/query.php"
+                    $.ajaxSetup({async: false});
 
-            // TODO: Add new info to database
+                    $.post(serv, { query: "SELECT password FROM users WHERE userid='" + result.user_id + "';"}, function(result) {
+                        var obj = JSON.parse(result);
+                        var password = obj[0].password;	
+                        if(password == currPass) {
+                            alert("Current password matches: " + password + " " + currPass);
+                        } else {
+                            alert("Current password does not match: " + password + " " + currPass);
+                        }
+                    });
+                }
+            });
+            alert("enterOk set to true");
+            enterOk = "true";
         }
    
         if (enterOk == "true"){
+            alert("enterOk = true");
+            chrome.storage.sync.get("user_id", function(result) {
+                if(!chrome.runtime.error) {
+                    alert("is this working");
+                    var serv = "http://freeflow.tk/query.php"
+                    $.ajaxSetup({async: false});
+
+                    $.post(serv, { query: "UPDATE users SET password='" + pass1 +"' WHERE userid='" + user_id + "';"});
+                    alert("password updated");
+                }
+            });
             window.location.href="/accountpage/accountpage.html";
         }
         else{
