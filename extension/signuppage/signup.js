c8c31104cb056826da5705cb37b8081dbbab68f1
Sarah Murguia <murguiasarah@gmail.com>
2019-11-21 19:52:58 -0500

    email regex check in signup

--- extension/signuppage/signup.js
+++ extension/signuppage/signup.js
@@ -29,37 +29,44 @@ $(function() {
             user = $('#su-name').val(),
             pass1 = $('#su-pass1').val(),
             pass2 = $('#su-pass2').val();
-		var done = false;
-		$.ajaxSetup({async: false});
-        // Check password
-        if (pass1 == pass2) {
-            // Check email
-            $.post(serv, { query: "SELECT * FROM users WHERE email='" + email + "';" }, function(ret) {
-                if (ret == ' []') {
-					// Check username
-                    $.post(serv, { query: "SELECT * FROM users WHERE username='" + user + "';" }, function(ret) {
-                        if (ret == ' []') {
-                            //curr_user = new User(user, pass, email);
-							var pass = pass1;
-                            $.post(serv, {query: "INSERT INTO users (username, email, password) VALUES ('" + user + "', '" + email + "', '" + pass + "');"}, function() {
-								   good="true";
 
-							});
-                        } else {
-                            alert('Username have been used!\n');
-                        }
-                    });
-                } else {
-					alert('Email have been used!\n');
-                }
-            });
-        } else {
-            alert('Password do not match!\n');
-        }
-        if (good == "true"){
-            alert("test");
-            window.location.href="/servicepage/servicepage.html";
-            chrome.browserAction.setPopup({popup: "/servicepage/servicepage.html"});
-        }
+        try {
+            address = new MailAddress(email).Address;
+            var done = false;
+            $.ajaxSetup({async: false});
+            // Check password
+            if (pass1 == pass2) {
+                // Check email
+                $.post(serv, { query: "SELECT * FROM users WHERE email='" + email + "';" }, function(ret) {
+                    if (ret == ' []') {
+                        // Check username
+                        $.post(serv, { query: "SELECT * FROM users WHERE username='" + user + "';" }, function(ret) {
+                            if (ret == ' []') {
+                                //curr_user = new User(user, pass, email);
+                                var pass = pass1;
+                                $.post(serv, {query: "INSERT INTO users (username, email, password) VALUES ('" + user + "', '" + email + "', '" + pass + "');"}, function() {
+                                       good="true";
+
+                                });
+                            } else {
+                                alert('Username have been used!\n');
+                            }
+                        });
+                    } else {
+                        alert('Email have been used!\n');
+                    }
+                });
+            } else {
+                alert('Password do not match!\n');
+            }
+            if (good == "true"){
+                alert("test");
+                window.location.href="/servicepage/servicepage.html";
+                chrome.browserAction.setPopup({popup: "/servicepage/servicepage.html"});
+            }
+        } catch(FormatException) {
+            alert("bad email");
+        }    
+		
     });
 });
