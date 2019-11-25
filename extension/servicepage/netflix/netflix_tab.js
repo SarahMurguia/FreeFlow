5e0cb31f6c51785ca28128afa6634d3185d6e9f6
riosm2 <myron2277@gmail.com>
2019-11-25 14:22:55 -0500

    fixed email as password and also pre filled text bug

--- extension/servicepage/netflix/netflix_tab.js
+++ extension/servicepage/netflix/netflix_tab.js
@@ -2,18 +2,18 @@ function addEmailLetter(i) {
     setTimeout(function() {document.getElementById('id_userLoginId').value += email.charAt(i);}, i*9 + 300);
 }
 function addPassLetter(i, max) {
-    setTimeout(function() {document.getElementById('id_password').value += email.charAt(i);}, i*9 + max*12 + 300);
+    setTimeout(function() {document.getElementById('id_password').value += pass.charAt(i);}, i*9 + max*12 + 300);
 }
 
 
-document.getElementById('id_userLoginId').value = '';
-document.getElementById('id_password').value = '';
-
+document.getElementById("id_userLoginId").className = "nfTextField hasText";
 for (var i = 0; i < email.length; i++) {
     addEmailLetter(i);
 }
-for (var i = 0; i < email.length; i++) {
-    addPassLetter(i, email.length);
+
+document.getElementById("id_password").className = "nfTextField hasText";
+for (var i = 0; i < pass.length; i++) {
+    addPassLetter(i, pass.length);
 }
 
 document.getElementById('bxid_rememberMe_true').value = false;
