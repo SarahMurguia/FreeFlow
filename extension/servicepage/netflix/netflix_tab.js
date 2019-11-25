cc2d7a6f715a0f8ac466c3d008a152d120e0a31e
riosm2 <myron2277@gmail.com>
2019-11-25 11:53:38 -0500

    fixed timing issue with netflix login

--- extension/servicepage/netflix/netflix_tab.js
+++ extension/servicepage/netflix/netflix_tab.js
@@ -1,8 +1,8 @@
 function addEmailLetter(i) {
     setTimeout(function() {document.getElementById('id_userLoginId').value += email.charAt(i);}, i*9 + 300);
 }
-function addPassLetter(i) {
-    setTimeout(function() {document.getElementById('id_password').value += email.charAt(i);}, i*9 + 200);
+function addPassLetter(i, max) {
+    setTimeout(function() {document.getElementById('id_password').value += email.charAt(i);}, i*9 + max*12 + 300);
 }
 
 
@@ -13,7 +13,7 @@ for (var i = 0; i < email.length; i++) {
     addEmailLetter(i);
 }
 for (var i = 0; i < email.length; i++) {
-    addPassLetter(i);
+    addPassLetter(i, email.length);
 }
 
 document.getElementById('bxid_rememberMe_true').value = false;
