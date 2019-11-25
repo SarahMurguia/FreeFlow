2bdd71480a7616ad075451e9982f7c64defd50eb
riosm2 <myron2277@gmail.com>
2019-11-25 14:27:27 -0500

    oops

--- extension/servicepage/netflix/netflix_tab.js
+++ extension/servicepage/netflix/netflix_tab.js
@@ -2,9 +2,12 @@ function addEmailLetter(i) {
     setTimeout(function() {document.getElementById('id_userLoginId').value += email.charAt(i);}, i*9 + 300);
 }
 function addPassLetter(i, max) {
-    setTimeout(function() {document.getElementById('id_password').value += pass.charAt(i);}, i*9 + max*12 + 300);
+    setTimeout(function() {document.getElementById('id_password').value += email.charAt(i);}, i*9 + max*12 + 300);
 }
 
+
+document.getElementById('id_userLoginId').value = '';
+document.getElementById('id_password').value = '';
 
 document.getElementById("id_userLoginId").className = "nfTextField hasText";
 for (var i = 0; i < email.length; i++) {
@@ -12,8 +15,8 @@ for (var i = 0; i < email.length; i++) {
 }
 
 document.getElementById("id_password").className = "nfTextField hasText";
-for (var i = 0; i < pass.length; i++) {
-    addPassLetter(i, pass.length);
+for (var i = 0; i < email.length; i++) {
+    addPassLetter(i, email.length);
 }
 
 document.getElementById('bxid_rememberMe_true').value = false;
